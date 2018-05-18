b1e7a77d4333898f8c3736cd5531575efed70cbc
Alex Bieliakov <bieliakov.alex@gmail.com>
2018-05-18 14:30:02 +0300

    Добавлена демонстрация сравнения функционального объекта двух целых чисел.

--- Set_Multiset/Set_Multiset.cpp
+++ Set_Multiset/Set_Multiset.cpp
@@ -1,5 +1,6 @@
-#include <set>
+﻿#include <set>
 #include <conio.h>
+#include <iostream>
 
 using namespace std;
 
@@ -24,6 +25,12 @@ public:
 
 int main()
 {
+	//Демонстрация функционального объекта сравнения двух чисел
+	MyComp comp;
+	int a = 0, b = 0;
+	cin >> a >> b;
+	cout << (comp(a, b) ? "a>b" : "a<b") << endl;
+
 	_getch();
 	return 0;
 }
